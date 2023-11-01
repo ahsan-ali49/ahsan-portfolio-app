0e5b39b03aafa83110581ff3f32da6ac8dab3f47
Ahsan Ali Shah <ahsanali.bscssef20@iba-suk.edu.pk>
2023-11-01 12:40:03 +0500

    added hero-section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,17 +5,17 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 
 function Header() {
   return (
-    <Navbar expand="lg">
+    <Navbar expand="lg p-3" className="header-section navbar-dark">
       <Container>
         <Navbar.Brand href="#home">AHSAN</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
             <Nav.Link href="#home">HOME</Nav.Link>
-            <Nav.Link href="#link">ABOUT</Nav.Link>
-            <Nav.Link href="#link">PROJECTS</Nav.Link>
-            <Nav.Link href="#link">BLOG</Nav.Link>
-            <Nav.Link href="#link">REVIEWS</Nav.Link>
+            <Nav.Link href="#about">ABOUT</Nav.Link>
+            <Nav.Link href="#projects">PROJECTS</Nav.Link>
+            <Nav.Link href="#blog">BLOG</Nav.Link>
+            <Nav.Link href="#reviews">REVIEWS</Nav.Link>
             <NavDropdown title="SERVICES" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Frontend</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">Backend</NavDropdown.Item>
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,7 +1,7 @@
 export const HeroSection = () => {
   return (
-    <div className="text-center">
-      <h1 className="mt-5 nameHeader">Hi! I'm Ahsan Shah</h1>
+    <div className="text-center hero-section">
+      <h1 className="nameHeader">Hi! I'm Ahsan Shah</h1>
       <h2>A Professional Web Developer.</h2>
     </div>
   );
