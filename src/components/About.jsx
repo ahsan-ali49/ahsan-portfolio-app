2ca9380cf14c03bc72519a4dc44fb21c5409603c
Ahsan Ali Shah <ahsanali.bscssef20@iba-suk.edu.pk>
2023-11-03 23:37:11 +0500

    added name and bio

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,14 +10,26 @@ export const About = () => {
     padding: "0",
   };
   return (
-    <div style={mystyle} className="container-fluid p-5">
-      <div className="row justify-content-center">
+    <div style={mystyle} className="container-fluid">
+      <div className="row justify-content-center p-5">
         <img
           style={imageStyles}
           className="rounded-circle border border-5 border-warning"
           src={dpimage}
-          alt="asdf"
+          alt="my display picture"
         />
+      </div>
+      <div style={mystyle} className="row">
+        <div className="col ps-5">
+          <h2>Hi There! I'm Ahsan Shah</h2>
+          <p className="w-75 text-secondary">
+            I'm 22 years old creative web designer based in Pakistan,
+            specializing in User Interface Design and Development. I build
+            clean, appealing, and functional interfaces which comply with the
+            latest web standards.
+          </p>
+        </div>
+        <div className="col">Hello2</div>
       </div>
     </div>
   );
