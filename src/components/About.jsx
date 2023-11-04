0b916192d846809501a5b7db0b2a60827dbe1a7a
Ahsan Ali Shah <ahsanali.bscssef20@iba-suk.edu.pk>
2023-11-04 20:27:04 +0500

    added bio icons

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,9 @@
 import dpimage from "./dp.JPG";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+import LocalPhoneIcon from "@mui/icons-material/LocalPhone";
+import EmailIcon from "@mui/icons-material/Email";
+
 export const About = () => {
   const mystyle = {
     height: "80vh",
@@ -16,7 +21,7 @@ export const About = () => {
           style={imageStyles}
           className="rounded-circle border border-5 border-warning"
           src={dpimage}
-          alt="my display picture"
+          alt="my display"
         />
       </div>
       <div style={mystyle} className="row">
@@ -28,6 +33,27 @@ export const About = () => {
             clean, appealing, and functional interfaces which comply with the
             latest web standards.
           </p>
+          <div className="row mt-5">
+            <div className="col">
+              <LocationOnIcon className="border border-1 border-dark rounded-circle p-1 float-clear" />
+              <span> Location : SIBA, Sukkur, Pakistan.</span>
+            </div>
+            <div className="col">
+              <CalendarMonthIcon className="border border-1 border-dark rounded-circle p-1" />
+              <span> Age : 32</span>
+            </div>
+          </div>
+
+          <div className="row mt-2">
+            <div className="col">
+              <LocalPhoneIcon className="border border-1 border-dark rounded-circle p-1" />
+              <span> Phone : +92-3163319605</span>
+            </div>
+            <div className="col">
+              <EmailIcon className="border border-1 border-dark rounded-circle p-1" />
+              <span> Email : ahsan.shah330@gmail.com</span>
+            </div>
+          </div>
         </div>
         <div className="col">Hello2</div>
       </div>
