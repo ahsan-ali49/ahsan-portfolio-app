cbe29b9f37548f324059a8ad43d3268d4c1e4c63
Ahsan Shah <ahsanali.bscssef20@iba-suk.edu.pk>
2024-11-20 18:41:27 +0500

    tab changing

--- src/app/_components/TabNavigation.jsx
+++ src/app/_components/TabNavigation.jsx
@@ -5,23 +5,27 @@ import React, { useState } from "react";
 const TabNavigation = () => {
   const [activeTab, setActiveTab] = useState("Services");
 
-  const tabs = ["About", "Projects", "Services"];
+  const tabs = [
+    { label: "About", link: "about" },
+    { label: "Projects", link: "projects" },
+    { label: "Services", link: "/" },
+  ];
 
   return (
     <div className="md:px-[140px] md:mb-[30px]">
       <div className="tab-navigation-bar w-fit">
-        {tabs.map((tab) => (
+        {tabs.map((tab, index) => (
           <Link
-            key={tab}
-            href={tab.toLocaleLowerCase()}
-            // onClick={() => setActiveTab(tab)}
+            key={index}
+            href={tab.link}
+            onClick={() => setActiveTab(tab.label)}
             className={`transition ${
-              activeTab === tab
+              activeTab === tab.label
                 ? "navigation-button-active inter-black-20-500"
                 : "navigation-button-inactive inter-white-20-500"
             }`}
           >
-            {tab}
+            {tab.label}
           </Link>
         ))}
       </div>
